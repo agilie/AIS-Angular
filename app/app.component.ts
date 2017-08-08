f1a387d32880e0e08c79504a0504c4d781e3f9b0
SergeyMell <sergey.mell@agilie.com>
2017-08-08 18:24:17 +0300

    Using Http Events

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {ApiService} from "./api.service";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpEventType} from "@angular/common/http";
 
 @Component({
     selector: 'my-app',
@@ -14,8 +14,18 @@ export class MyComponent {
     constructor(private api: ApiService, http: HttpClient) {
         api.get('/api/users');
 
-        http.get('https://jsonplaceholder.typicode.com/posts', {observe: 'response'})
-            .subscribe(resp => this.posts = resp.body);
+        http.get('https://eoimages.gsfc.nasa.gov/images/imagerecords/73000/73751/world.topo.bathy.200407.3x21600x21600.D2.png',
+            {
+                observe: 'events',
+                reportProgress: true
+            })
+            .subscribe(resp => {
+                if (resp.type == HttpEventType.DownloadProgress) {
+                    console.log(`Loaded: ${Math.round( 100 *resp.loaded / resp.total)}`);
+                } else if (resp.type == 4) {
+                    console.log('All done');
+                }
+            });
     }
 
 }
