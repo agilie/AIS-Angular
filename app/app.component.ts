9b035f910857cbe56d24e86364913df685add9ed
SergeyMell <sergey.mell@agilie.com>
2017-08-08 18:10:30 +0300

    Implemented interceptor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,21 @@
 import {Component} from '@angular/core';
 import {ApiService} from "./api.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'my-app',
-    template: '<h1> {{ myDate | fromNow:true }}</h1>'
+    template: '<pre *ngFor="let post of posts">{{post | json}}</pre>'
 })
 
 export class MyComponent {
 
-    myDate = Date.parse('10.10.2015');
-    
-    constructor(private api: ApiService) {
+    posts: any = [];
+
+    constructor(private api: ApiService, http: HttpClient) {
         api.get('/api/users');
+
+        http.get('https://jsonplaceholder.typicode.com/posts', {observe: 'response'})
+            .subscribe(resp => this.posts = resp.body);
     }
 
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,13 +4,16 @@ import {MyComponent} from "./app.component";
 import {ApiService} from "./api.service";
 import {FakeService} from "./fake.service";
 import {PipesModule} from "./pipes.module";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {Tokenizer} from "./api.interceptor";
 
 const TEST_MODE = false;
 
 @NgModule({
-    imports: [BrowserModule, PipesModule],
+    imports: [BrowserModule, PipesModule, HttpClientModule],
     declarations: [MyComponent],
     providers: [
+        {provide: HTTP_INTERCEPTORS, useClass: Tokenizer, multi: true},
         {provide: ApiService, useFactory: () => new ApiService('http://dev.domain.com')},
     ],
     bootstrap: [MyComponent]
