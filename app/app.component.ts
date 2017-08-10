9f82b60d167b9a0ceb28fd129a14aaea7750ca6e
SergeyMell <sergey.mell@agilie.com>
2017-08-10 18:20:25 +0300

    Initial commit for routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,31 +2,16 @@ import {Component, ViewChild} from '@angular/core';
 import {ApiService} from "./api.service";
 import {HttpClient, HttpEventType} from "@angular/common/http";
 
-import * as jQuery from 'jquery';
-import 'select2';
-import 'select2/dist/css/select2.css'
-
 @Component({
     selector: 'my-app',
-    template: `
-        <select #select>
-            <option *ngFor="let post of posts" [value]="post">{{post}}</option>
-        </select>
-    `
+    template: ``
 })
 
 export class MyComponent {
-
-    @ViewChild('select') select;
-
-    posts: any = ['First', 'Very long element', 'Third', 'Fourth', 'Fifth'];
 
     constructor() {
 
     }
 
-    ngAfterViewInit() {
-        jQuery(this.select.nativeElement).select2();
-    }
 
 }
