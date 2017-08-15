87e5437157f4c451d4efe140a433122e03f502d9
SergeyMell <sergey.mell@agilie.com>
2017-08-15 18:48:38 +0300

    Implemented Dashboard state

--- app/dashboard/dashboard.component.ts
+++ app/dashboard/dashboard.component.ts
@@ -1,10 +1,26 @@
 import {Component} from "@angular/core";
+import {UserModel} from "../models/user.model";
 
 @Component({
     selector: 'app-dashboard',
-    template: '<div>I am dashboard</div>'
+    template: `<div *ngFor="let photo of userPhotos">
+        <img [src]="photo.picture" alt="">
+    </div>`
 })
 export class DashboardComponent {
+
+    userPhotos:any = [];
+
+    constructor(private user: UserModel) {
+
+    }
+
+    ngOnInit() {
+        this.user.getMe().subscribe(
+            response => this.user.getUserPhotos(response['id'])
+                .subscribe(response => this.userPhotos = response['data'])
+        )
+    }
 
 
 }
--- app/models/user.model.ts
+++ app/models/user.model.ts
@@ -1,9 +1,14 @@
 import {Injectable} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
 
 @Injectable()
 export class UserModel {
 
     private fbToken;
+
+    constructor(private http: HttpClient) {
+
+    }
 
     get facebookToken() {
         return this.fbToken;
@@ -11,6 +16,21 @@ export class UserModel {
 
     set facebookToken(token) {
         this.fbToken = token;
+    }
+
+    getMe() {
+        let params = new HttpParams()
+            .set('access_token', this.facebookToken)
+            .set('format', 'json');
+        return this.http.get('https://graph.facebook.com/v2.10/me', {params})
+    }
+
+    getUserPhotos(user_id) {
+        let params = new HttpParams()
+            .set('access_token', this.facebookToken)
+            .set('fields', 'picture')
+            .set('format', 'json');
+        return this.http.get(`https://graph.facebook.com/v2.10/${user_id}/photos`, {params});
     }
 
 }
